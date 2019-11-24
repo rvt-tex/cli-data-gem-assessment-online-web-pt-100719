d973efd3836df17364487307ccb72155de05bccf
github username <github email address>
2019-11-24 14:35:54 -0500

    created a class variable @@cocktail_letter, get_cocktails_db method & get data from the api & creating objects with that data

--- Kocktailz/lib/Kocktailz/api.rb
+++ Kocktailz/lib/Kocktailz/api.rb
@@ -2,5 +2,15 @@ class Kocktailz::API
 
     ROOT_URL= 'https://www.thecocktaildb.com/api/json/v1/1/search.php?'
 
+    @@cocktail_letter = ''
 
+        def self.get_cocktails_db(query)
+           @@cocktail_letter = ('a'..'z')    #.to_a.sample
+
+        response = HTTParty.get(ROOT_URL + "f=#{query}")
+        
+        response["drinks"].each do |drinks_hash|
+            Kocktailz::Cocktails.new(drinks_hash)
+        end 
+        end 
 end 
