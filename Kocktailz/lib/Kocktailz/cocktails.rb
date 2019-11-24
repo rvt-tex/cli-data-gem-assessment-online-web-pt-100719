730973041c5caaff29d3a96a939df8c5b36b1533
github username <github email address>
2019-11-24 14:41:47 -0500

    created initialize method taking args, created class variable @@all set to empty array, def self.all and also shoveled self in @@all

--- Kocktailz/lib/Kocktailz/cocktails.rb
+++ Kocktailz/lib/Kocktailz/cocktails.rb
@@ -1,5 +1,16 @@
 class Kocktailz::Cocktails
 
+    attr_accessor :strDrink, :idDrink, :strCategory
+ 
+    @@all = []
 
+    def initialize(args)
+        update(args)
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+    
 end 
