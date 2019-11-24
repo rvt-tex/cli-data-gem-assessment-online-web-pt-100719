78d09cf680f31bdc7cb866aa1a32ec17718c1ec8
github username <github email address>
2019-11-24 13:36:27 -0500

    required api, cli and cocktail class files in my environment

--- Kocktailz/lib/environment.rb
+++ Kocktailz/lib/environment.rb
@@ -3,9 +3,9 @@ require 'json'
 require 'HTTParty'
 require 'rest-client'
 
-
-
-
 require "Kocktailz/version"
+require_relative "./kocktailz/api"
+require_relative "./kocktailz/cli"
+require_relative "./kocktailz/cocktails"
 
 
