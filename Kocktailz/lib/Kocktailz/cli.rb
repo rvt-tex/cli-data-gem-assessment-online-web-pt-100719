b9e4dc820873b7d2965145948d0d5e19e901bc11
github username <github email address>
2019-11-29 13:38:36 -0500

    modified more_info by increasing the amount of attributes displayed

--- Kocktailz/lib/Kocktailz/cli.rb
+++ Kocktailz/lib/Kocktailz/cli.rb
@@ -49,24 +49,21 @@ class Kocktailz::CLI
     end 
 
     def more_info
-        puts ""
         puts "Enter the number you would like to view more information on?:"
-
-            input = gets.strip.downcase
-        puts ""
+        input = gets.chomp.downcase
         cocktail = Kocktailz::Cocktails.all[input.to_i - 1]
-        Kocktailz::API.get_single_cocktail(cocktail)
+        Kocktailz::API.get_selected_cocktail(cocktail)
 
         puts "strDrink: #{cocktail.strDrink}"
         puts "idDrink: #{cocktail.idDrink}"
         puts "strCategory: #{cocktail.strCategory}"
-        puts "strAlcoholic: #{cocktail.strAlcoholic}"
-        #puts "strGlass: #{cocktail.strGlass}"
+        puts "strIngredient1: #{cocktail.strIngredient1}"
+        puts "strIngredient2: #{cocktail.strIngredient2}"
+        puts "strIngredient3: #{cocktail.strIngredient3}"
         puts "strInstructions: #{cocktail.strInstructions}"
-        #puts "strIngredient1: #{cocktail.strIngredient1}"
-        #puts "strIngredient2: #{cocktail.strIngredient2}"
-        #puts "strIngredient3: #{cocktail.strIngredient3}"
-    end
+        puts "strGlass: #{cocktail.strGlass}"
+        puts "strAlcoholic: #{cocktail.strAlcoholic}"
+    end 
 
     def goodbye
         puts "Would you like to search for another cocktail? Enter Y or N: "
