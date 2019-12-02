90adc127e9b420ffdf53e1ff4bef72d0cb9c7f60
github username <github email address>
2019-12-02 14:43:00 -0500

    deleted binding pry's and align code to look better

--- Kocktailz/lib/Kocktailz/api.rb
+++ Kocktailz/lib/Kocktailz/api.rb
@@ -5,23 +5,18 @@ class Kocktailz::API
     @@cocktail_letter = ''
         
         def self.get_cocktails_db(query)
-           @@cocktail_letter = ('a'..'z')    #.to_a.sample
+           @@cocktail_letter = ('a'..'z') 
         
         response = HTTParty.get(ROOT_URL + "f=#{query}")
-        #binding.pry
-        #need to put in conditaional to check for empty hash, null returns
         response["drinks"].each do |drinks_hash|
             Kocktailz::Cocktails.new(drinks_hash)
         end 
-        #binding.pry
         end 
 
         def self.get_single_cocktail(cocktail)
-
             if !cocktail.strDrink 
             response = HTTParty.get(ROOT_URL + "s=#{cocktail}")
             Kocktailz::Cocktails.update(response)
             end 
-        end 
-       #binding.pry
+        end
 end 
