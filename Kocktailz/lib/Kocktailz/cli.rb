788e6ec3e34c2f21a0d3074f29d947c18ea94a9d
github username <github email address>
2019-12-02 19:02:17 -0500

    created a if statement to check to see if user had inputed exit, and rearrage some of the code logic in another method another_search

--- Kocktailz/lib/Kocktailz/cli.rb
+++ Kocktailz/lib/Kocktailz/cli.rb
@@ -25,6 +25,7 @@ class Kocktailz::CLI
     greet_user
     list_cocktails 
     more_info
+    another_search
     goodbye
     end 
 
@@ -42,16 +43,18 @@ class Kocktailz::CLI
 
     def list_cocktails   
         input = gets.chomp.downcase  
-        puts ""
-        if !invalidate_input?(input)
+        if input === "exit"
+        goodbye
+        elsif !invalidate_input?(input)
             Kocktailz::API.get_cocktails_db(input)
+            
             Kocktailz::Cocktails.all.each.with_index do |d, i|
                 puts "#{i + 1}. #{d.strDrink}"
             end         
         else
             puts "Sorry that is not a Valid Option, please try another letter!"
             list_cocktails
-        end 
+        end
     end     
     
 
@@ -59,8 +62,10 @@ class Kocktailz::CLI
         puts ""
         puts "Enter the number you would like to view more information on?:"
         puts ""
-        input = gets.chomp
-        if (1..Kocktailz::Cocktails.all.size).include?(input.to_i)
+        input = gets.chomp 
+        if input === "exit"
+        goodbye
+        elsif (1..Kocktailz::Cocktails.all.size).include?(input.to_i)
     
         cocktail = Kocktailz::Cocktails.all[input.to_i - 1]
         Kocktailz::API.get_single_cocktail(cocktail)
@@ -77,28 +82,36 @@ class Kocktailz::CLI
         else 
             puts "Sorry that is not a valid option, please try a different number"
             more_info
-        end 
+        end
     end 
 
-    def goodbye
+    def another_search
+        puts "Would you like to search for another cocktail? Enter Y or N"
+           input = gets.chomp.downcase 
+        if input === "exit"
+            goodbye
+        end
+
         puts ""
-        puts "Would you like to search for another cocktail? Enter Y or N or EXIT: "
-        input = gets.strip.downcase
         case input
         when "y"
             puts "Type the first letter of the cocktail you would like to search:"  
             Kocktailz::Cocktails.all.clear  
             list_cocktails 
             more_info
+            another_search
             goodbye
         when "n"
-            puts "Thanks for using Kocktailz!!!"
-            puts "Tequila may not be the answer, but it's worth a shot."
-        when "exit"
-            puts "Thanks for using Kocktailz!!!"
-            puts "Tequila may not be the answer, but it's worth a shot."
+            goodbye
         else
-            puts "I don't understand that answer. Enter Y or N or EXIT"
+            puts "I don't understand that answer. Enter Y or N"
         end
+    end 
+        
+
+    def goodbye
+        puts "Thanks for using Kocktailz!!!"
+        puts "Tequila may not be the answer, but it's worth a shot."
+        exit
     end 
 end 
