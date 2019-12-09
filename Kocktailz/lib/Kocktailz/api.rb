df2e6931373df3668204027753962d521b4a7b1a
github username <github email address>
2019-12-09 18:16:52 -0500

    fixed indentation

--- Kocktailz/lib/Kocktailz/api.rb
+++ Kocktailz/lib/Kocktailz/api.rb
@@ -7,16 +7,21 @@ class Kocktailz::API
         def self.get_cocktails_db(query)
            @@cocktail_letter = ('a'..'z') 
         
-        response = HTTParty.get(ROOT_URL + "f=#{query}")
-        response["drinks"].each do |drinks_hash|
-            Kocktailz::Cocktails.new(drinks_hash)
-        end 
+            response = HTTParty.get(ROOT_URL + "f=#{query}")
+            response["drinks"].each do |drinks_hash|
+                Kocktailz::Cocktails.new(drinks_hash)
+            end 
+            
+
         end 
 
         def self.get_single_cocktail(cocktail)
             if !cocktail.strDrink 
-            response = HTTParty.get(ROOT_URL + "s=#{cocktail}")
-            Kocktailz::Cocktails.update(response)
+                response = HTTParty.get(ROOT_URL + "s=#{cocktail}")
+               
+                Kocktailz::Cocktails.update(response)
+                
             end 
         end
 end 
+
